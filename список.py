0e123400b7a13c3425755759b1c74069a249cb93
Maruf995 <82045849+Maruf995@users.noreply.github.com>
2021-07-07 17:16:44 +0600

    Add files via upload

---
+++ список.py
@@ -0,0 +1,17 @@
+print("   Список Продуктов     ")
+products = ["Вода", "Хлеб", "Овощи"]
+print(products)
+while True:
+    delete = input("Добавить [1], Убрать [2] ")
+    if delete == "1":
+        print("Какой продукт добавить? ")
+        product = input()
+        products.append(product)
+        print(products)
+    if delete == "2":
+        print("Какой продукт убрать? ")
+        product = input()
+        products.remove(product)
+        print(products)
+    else:
+        print("Не верное число!")
